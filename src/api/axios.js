5e632a9df82406c6c31c703dcb143cff747bb29d
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-03-02 16:24:56 +0200

    Commented api and button

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/**
+ * Used to create own request format for the API that this application uses.
+ */
 export default axios.create({
     baseURL: "https://moviesoftwareapi.herokuapp.com/api/"
 });
--- src/api/poster.js
+++ src/api/poster.js
@@ -1,3 +1,7 @@
+/**
+ * Template string for The Movie Database, to fetch a poster for a specific movie.
+ * @type {string} First half of the url
+ */
 const POSTER_URL = "https://image.tmdb.org/t/p/original/";
 
 export default POSTER_URL;
